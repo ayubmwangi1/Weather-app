959edd1433394a8c443b3a2549d0485c7e64094b
ayubmwangi1 <ayubmwangi1@gmail.com>
2026-02-17 12:34:53 +0300

    built the weather card and made it responsive

--- weathernow/src/components/CurrentWeather.jsx
+++ weathernow/src/components/CurrentWeather.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
 
-export default function CurrentWeather() {
+import WeatherIcon from "./WeatherIcon";
+import { formatDate } from "../lib/utils";
+
+export default function CurrentWeather({ data, location }) {
+  if (!data) return null;
+
+  const currentTemp = Math.round(data.current.temperature_2m);
+  const weatherCode = data.current.weather_code;
+
   return (
-    <div>CurrentWeather</div>
-  )
+    <div className="weather-bg rounded-2xl px-6 py-24 relative overflow-hidden">
+      <div className="flex flex-col sm:flex-row items-center justify-between relative z-10">
+        <div className="mb-4">
+          <p className="text-2xl font-bold text-white mb-2">{location.name}</p>
+          <p className="text-blue-100 text-base">{formatDate(new Date())}</p>
+        </div>
+
+        <div className="flex items-center space-x-4">
+          <WeatherIcon code={weatherCode} size="large" />
+          <div className="ml-8 text-8xl font-bold text-white text-shadow italic">
+            {currentTemp}&deg;
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
