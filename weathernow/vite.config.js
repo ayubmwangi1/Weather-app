cbcc5adbeda96f3c3154d0785c2041b5d042ec04
ayubmwangi1 <ayubmwangi1@gmail.com>
2026-02-27 18:26:14 +0300

    refactor the deploy

--- weathernow/vite.config.js
+++ weathernow/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base:"/Weather-app/",
+  base:process.env.VITE_BASE_PATH || "/Weather-app/",
 })
